25bc75ff8005a95e9267fd1dd4acba5e324bfa84
adodoCoder <alavropulido@my.whccd.edu>
2026-04-06 16:41:13 -0700

    Add project files.

---
+++ COMP003B.LectureActivity3/Program.cs
@@ -0,0 +1,44 @@
+/*
+ * Author: Alvaro Pulido
+ * Course: COMP-003B: ASP.NET Core
+ * Faculty: Jonathan Cruz
+ * Purpose: Demonstrates routing, model binding, and validation in ASP.NET Core
+ */
+namespace COMP003B.LectureActivity3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            
+            // Conventional routing defaults to the Home controler and Index action.
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
